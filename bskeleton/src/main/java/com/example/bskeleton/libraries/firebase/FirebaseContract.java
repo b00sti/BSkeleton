c3faddf56762cfafa556ed15271fb412b1d33f3b
b00sti <pawlik.dominik@gmail.com>
2017-02-14 00:36:22 +0100

    added firebase

---
+++ bskeleton/src/main/java/com/example/bskeleton/libraries/firebase/FirebaseContract.java
@@ -0,0 +1,9 @@
+package com.example.bskeleton.libraries.firebase;
+
+/**
+ * Created by Dominik (b00sti) Pawlik on 2016-11-10
+ */
+
+public class FirebaseContract {
+
+}
---
+++ bskeleton/src/main/java/com/example/bskeleton/libraries/firebase/FirebaseUtils.java
@@ -0,0 +1,9 @@
+package com.example.bskeleton.libraries.firebase;
+
+/**
+ * Created by Dominik (b00sti) Pawlik on 2016-11-03
+ */
+
+public class FirebaseUtils {
+
+}
